0ca0560f92409964d5d3a70d041a3e88417248c1
Thor <github@iskalt.is>
2023-12-15 10:35:25 +0100

    Graphs now extend 7 days beyond data points. Trend lines now extend beyond data points.

--- src/plot_graph.py
+++ src/plot_graph.py
@@ -3,19 +3,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def plot_graph(title, weights, predictions, increases, decreases, days, labels, output):
-
+    
     # Create a figure
     plot = plt.figure(figsize=(10, 5))
 
     # Plot the data points for weights
     plt.scatter(days, weights, label='Weight', color='blue')
 
+    # Extend the range of x-values for trend lines
+    extended_days = np.arange(min(days), max(days) + 8, 1)
+
     # Calculate the trend line for weights using numpy's polyfit
     z_weights = np.polyfit(days, weights, 1)
     p_weights = np.poly1d(z_weights)
 
     # Plot the trend line for weights
-    plt.plot(days, p_weights(days), "--", label='Weight', color='blue')
+    plt.plot(extended_days, p_weights(extended_days), "--", label='Weight', color='blue')
 
     # Plot the data points for predictions
     plt.scatter(days, predictions, label='Expected weight', color='orange')
@@ -25,7 +28,7 @@ def plot_graph(title, weights, predictions, increases, decreases, days, labels, 
     p_predictions = np.poly1d(z_predictions)
 
     # Plot the trend line for predictions
-    plt.plot(days, p_predictions(days), "--", label='Expected weight loss', color='orange')
+    plt.plot(extended_days, p_predictions(extended_days), "--", label='Expected weight loss', color='orange')
 
     # Plot the data points for increases
     # plt.scatter(days, increases, label='Increase Data Points', color='red', marker='^')
@@ -35,7 +38,7 @@ def plot_graph(title, weights, predictions, increases, decreases, days, labels, 
     p_increases = np.poly1d(z_increases)
 
     # Plot the trend line for increases
-    plt.plot(days, p_increases(days), "--", label='500 kcal increase', color='red')
+    plt.plot(extended_days, p_increases(extended_days), "--", label='500 kcal increase', color='red')
 
     # Plot the data points for decreases
     # plt.scatter(days, decreases, label='Decrease Data Points', color='green', marker='v')
@@ -45,7 +48,7 @@ def plot_graph(title, weights, predictions, increases, decreases, days, labels, 
     p_decreases = np.poly1d(z_decreases)
 
     # Plot the trend line for decreases
-    plt.plot(days, p_decreases(days), "--", label='500 kcal decrease',  color='green')
+    plt.plot(extended_days, p_decreases(extended_days), "--", label='500 kcal decrease',  color='green')
 
     # Set plot title and labels
     plt.title(f'Day {title}')
@@ -55,7 +58,7 @@ def plot_graph(title, weights, predictions, increases, decreases, days, labels, 
     # Set grid and y-axis inversion
     plt.grid(axis='both')
     plt.yticks(range(min([int(n) for n in weights]) - 1, max([int(n) for n in weights]) + 1, 1))
-    plt.xticks(range(min(days), max(days) + 1, 1))
+    plt.xticks(range(min(days), max(days) + 8, 1))
 
     # Add legend
     plt.legend()
